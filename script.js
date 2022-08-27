c0e52db7d9c4aacc4063d8bbd12909f19647e685
kkatsouri <krystyna.katsouri@gmail.com>
2022-08-27 22:33:30 +1000

    wrote function for requesting password criteria input

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //Arrays to define what can can go into generated password
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var specialcharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '_', '-', '?'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '_', '-', '?'];
 var numeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var lengthOfPassword = 8;
+var criteria = []
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -19,23 +21,29 @@ function writePassword () {
  //Console says generatePassword is not defined. So here I am writing a function to define generatePassword
 function generatePassword() {
 
-//1. prompt the user, ask them to define what they want/password criteria
-
-
-//1.a Password length btwn 8-128, how to make sure? 
-
-//1.b lower case, uppercase, special characters? (how to define? set up variables? arrays?)
-
-
-
-
-//2 validate the input. (make sure input is btwn 8 and 128, and that at least uppercase or lowercase is selected)
-//3 generate the password 
-
-
-//4 display the generated password on the page
-return ["Generated password will go here"]
+//First use a function to ask user for length of password, use parseInt to change the returned string to an integar
+//then adding an if statement to make sure a number within 8-128 is entered, and is entered as digits not a string
+//then continue to use if statements to ask user to define other password criteria. 
+//combine var criteria with lowercase, uppercase etc. 
+function promptSelections() { 
+  lengthOfPassword = parseInt(window.prompt("Select password length. Enter number between 8 and 128"));
+ 
+  if((isNaN(lengthOfPassword)) || lengthOfPassword < 8 || lengthOfPassword > 128) {
+    alert("Please make sure you enter a number between 8 and 128");
+    return false; 
+  }
+  if (window.confirm ("Include lowercase?")){
+    criteria = criteria.concat(lowerCase);
+  }
+  if (window.confirm ("Include uppercase?")){
+    criteria = criteria.concat(upperCase);
+  }
+  if (window.confirm ("Include uppercase?")){
+    criteria = criteria.concat(upperCase);
+  }
+  return true;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -45,8 +53,3 @@ function writePassword() {
 
 }
 
-var lengthOfPassword = window.prompt ("Select password length. Enter number between 8 and 128"); 
-var lowercase = window.confirm ("Include lowercase?")
-var uppercase = window.confirm ("Include uppercase?")
-var numeric = window.confirm ("Include numbers?")
-var specialcharacters = window.confirm ("Include special characters?")
